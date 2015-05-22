81e1f23e8ad8d25286d6867d1a395b24e412506d
Eric Buckley <eric.buckley@gmail.com>
2015-05-22 17:25:50 -0400

    initial commit

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,39 @@
+<?php namespace App\Http\Controllers;
+
+use App\Article;
+use App\Http\Requests;
+use App\Http\Requests\CreateArticleRequest;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class ArticlesController extends Controller {
+
+	public function index()
+	{
+		$articles = Article::latest('published_at')->published()->get();
+
+		return view('articles.index', compact('articles'));
+	}
+
+	public function show($id)
+	{
+		$article = Article::findOrFail($id);
+
+		dd($article->published_at);
+
+		return view('articles.show', compact('article'));
+	}
+
+	public function create()
+	{
+		return view('articles.create');
+	}
+
+	public function store(CreateArticleRequest $request)
+	{
+		Article::create($request->all());
+
+		return redirect('articles');
+	}
+
+}
